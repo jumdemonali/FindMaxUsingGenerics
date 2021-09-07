b1d6c60ee340e2dd40c44b3a7aa0a14d3f789952
jumdemonali <jumdemonali@gmail.com>
2021-09-07 22:03:07 +0530

    Added: Prog to find max between three floats

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -7,12 +7,19 @@ public class Maximum {
         if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println("Maximum number is " + max);
+        System.out.println("Maximum integer value is " + max);
+        return max;
+    }
+    public static Float getMaximum(Float value1, Float value2, Float value3) {
+        Float max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        System.out.println("Maximum float value is " + max);
         return max;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum Program Using Generics");
-        Integer a = 10, b = 15, c = 12;
-        System.out.println(max(10,12,15));
     }
 }
--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -6,4 +6,10 @@ public class MaximumTest {
         int actualResult = Maximum.max(10, 15, 12);
         Assert.assertEquals(15, actualResult);
     }
+    @Test
+    public void givenThreeFloats_FindMaximumNumber(){
+        float actualValue = Maximum.getMaximum(1000f,545f,746f);
+        Assert.assertEquals(1000f,actualValue, 0);
+    }
+
 }
