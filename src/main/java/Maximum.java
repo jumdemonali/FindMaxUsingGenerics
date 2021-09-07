a774486a327db1ce16694b5feeeda5adb374a62f
jumdemonali <jumdemonali@gmail.com>
2021-09-07 20:53:35 +0530

    Added: Prog to find max between three integers

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,5 +1,18 @@
 public class Maximum {
+    public static Integer max(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("Maximum number is " + max);
+        return max;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum Program Using Generics");
+        Integer a = 10, b = 15, c = 12;
+        System.out.println(max(10,12,15));
     }
 }
--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -1,8 +1,9 @@
 import org.junit.Assert;
-//import org.junit.Test;
+import org.junit.Test;
 public class MaximumTest {
-   // @Test
+    @Test
     public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue() {
-
+        int actualResult = Maximum.max(10, 15, 12);
+        Assert.assertEquals(15, actualResult);
     }
 }
